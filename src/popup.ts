0dae4e7eda4664d5ec602a620e0ea99d6c42fdb3
piacib <ben.piacitelli@gmail.com>
2023-01-26 13:31:50 -1000

    added hyperlink to website

--- src/popup.ts
+++ src/popup.ts
@@ -3,3 +3,28 @@ import '../styles/popup.scss';
 document.getElementById('go-to-options').addEventListener('click', () => {
   chrome.runtime.openOptionsPage();
 });
+
+const query = { active: true, currentWindow: true };
+function callback(tabs: chrome.tabs.Tab[]) {
+  const currentTab = tabs[0]; // there will be only one in this array
+  const url = currentTab.url;
+  const pokeinfoUrl = 'https://piacib.github.io/pokeinfo/';
+  const pathname = url.match(/com\/(.*)/);
+  const anchor = document.getElementById('go-to-pokeinfo');
+  if (
+    url.includes('play.pokemon') &&
+    pathname &&
+    pathname[1] &&
+    pathname[1].includes('battle')
+  ) {
+    if (anchor instanceof HTMLAnchorElement) {
+      anchor.href = `${pokeinfoUrl}?battle=${pathname[1]}`;
+    }
+  } else {
+    if (anchor instanceof HTMLAnchorElement) {
+      anchor.href = pokeinfoUrl;
+    }
+  }
+}
+
+chrome.tabs.query(query, callback);
